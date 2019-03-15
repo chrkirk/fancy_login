84893d5a04ae7fc2d3a70fcf2d08a0770fe3c20d
chrkirk <christos.kirkos@gmail.com>
2019-03-15 22:17:29 +0200

    Updates

---
+++ precache-manifest.567dce9fd15b8cbbca3f2e1be64f0fc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d918a859c7b1dce9eb0e",
+    "url": "/login_and_register_stack_view/static/js/runtime~main.50ee8c47.js"
+  },
+  {
+    "revision": "4556784a44eac6c13c9f",
+    "url": "/login_and_register_stack_view/static/js/main.240e9477.chunk.js"
+  },
+  {
+    "revision": "2efc161a9861cfaaf948",
+    "url": "/login_and_register_stack_view/static/js/2.69395e53.chunk.js"
+  },
+  {
+    "revision": "4556784a44eac6c13c9f",
+    "url": "/login_and_register_stack_view/static/css/main.d38d959d.chunk.css"
+  },
+  {
+    "revision": "42030df6e2ddcb69d8712eca1d6454e2",
+    "url": "/login_and_register_stack_view/index.html"
+  }
+];
